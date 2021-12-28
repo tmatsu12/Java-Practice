d1ae64592e4d3c7dcf6ce106e4bd6605aa92f399
TakuMatsunaga <tmatsunaga65@gmail.com>
2021-12-28 19:16:26 +0900

    ITP1_6_Cの完了

--- AtCoder/IPT1/ITP1_6_C.java
+++ AtCoder/IPT1/ITP1_6_C.java
@@ -3,18 +3,27 @@ package AOJ;
 import java.util.Scanner;
 
 public class ITP1_6_C {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		int[] a = new int[n];
-		for(int i = 0; i < n; i ++) {
-			a[i] = scanner.nextInt();
-		}
-		for(int j = 0; j < n; j ++) {
-			if(j > 0) System.out.print(" ");
-			System.out.print(a[n-j-1]);
-		}
-		System.out.println();
-	}
+	public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        int num[][][]=new int[5][4][11];
+        int n=scan.nextInt();
+        for(int i=0;i<n;i++){
+            int b=scan.nextInt(),f=scan.nextInt(),r=scan.nextInt(),v=scan.nextInt();
+            num[b][f][r]+=v;
+        }
+        for(int i=1;i<=4;i++){
+            for(int j=1;j<=3;j++){
+                for(int k=1;k<=10;k++){
+                    System.out.print(" "+num[i][j][k]);
+                }
+                System.out.println();
+            }
+            if(i<4){
+                for(int j=0;j<20;j++)System.out.print('#');
+                System.out.println();
+            }
+        }
+        scan.close();
+    }
 
 }
